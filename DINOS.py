56b09991917fb9b68ef448a0bdd0536d173b951d
CamilaMartinGarrido <146133518+CamilaMartinGarrido@users.noreply.github.com>
2024-07-14 02:37:46 -0400

    Proyecto en python creado con importación de DAF.jar

---
+++ DINOS.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+
+# Ahora puedes importar clases de la biblioteca .jar
+from cu.edu.cujae.daf.context import Configuration
+
+# Función Objetivo
+
+config = Configuration.loadRawFile("./hyperparameters", '=')
+print(config)
+
+# Operadores para las S-MH
+
+
+
+# Operadores para la P-MH (1 de cruzamiento, 1 mutación)
+
+
+# Para ejecutar por terminal
+# # c:/jython2.7.3/bin/jython.exe "c:/3er año - 2do semestre/Opt. III Metaheurísticas/!! Opt III - MH/!!! Tareas/Tarea2-AilenCamilaCarlos/Tarea2_MH_DINOS/DINOS.py"
